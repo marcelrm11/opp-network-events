834442a209cf8eb9d216f5dc0efd2b5069e08365
marcelrm11 <marcelrm11@gmail.com>
2023-02-23 06:58:35 +0100

    frontend setup vuetify + router

--- frontend/django_events_frontend/src/App.vue
+++ frontend/django_events_frontend/src/App.vue
@@ -1,23 +1,30 @@
 <template>
-  <v-app>
-    <v-main>
-      <HelloWorld />
-    </v-main>
-  </v-app>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  name: "App",
+nav {
+  padding: 30px;
+}
 
-  components: {
-    HelloWorld,
-  },
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
 
-  data: () => ({
-    //
-  }),
-};
-</script>
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
